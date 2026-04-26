d6e4dfcbff3499e54fb4064760b392dd1e60cf7d
retnz <retnz80@gmail.com>
2026-04-26 13:46:49 +0100

    change the world verb to process only one world

--- WorldUpdate/MainWorldUpdate.cs
+++ WorldUpdate/MainWorldUpdate.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Design;
 using System.IO;
 using System.Text.Json;
-using Tiled2dot8.Entities;
 using Tiled2dot8.Mapper;
-using Tiled2dot8.Models;
 
 namespace Tiled2dot8
 {
@@ -13,27 +9,38 @@ namespace Tiled2dot8
     {
         public void Run(WorldOptions args)
         {
-            if (Directory.Exists(args.Input))
-            {
-                string[] worlds = Directory.GetFiles(Path.GetFullPath(args.Input), "*.world");
-                foreach (string worldFile in worlds)
-                {
-                    string worldRaw = File.ReadAllText(worldFile);
-                    Models.World worldData = JsonSerializer.Deserialize<Models.World>(worldRaw);
-                    Entities.World world = WorldMapper.Map(worldData);
-                    world.Name = Path.GetFileNameWithoutExtension(worldFile);
+            string worldRaw = File.ReadAllText(args.Input);
+            Models.World worldData = JsonSerializer.Deserialize<Models.World>(worldRaw);
+            Entities.World world = WorldMapper.Map(worldData);
+            world.Name = Path.GetFileNameWithoutExtension(args.Input);
 
-                    world.GetMatrix();
-                    ProcessWorld proc = new ProcessWorld(world);
-                    var worldProc = proc.Execute();
-                    string worldName = Path.GetFileName(worldFile);
-                    File.WriteAllText(Path.Combine(args.Output, Path.GetFileNameWithoutExtension(worldName) + "_worldMap.inc"), worldProc.ToString());
-                }
-            }
-            else
-            {
-                Console.WriteLine($"Path {args.Input} does not exist.");
-            }
+            world.GetMatrix();
+            ProcessWorld proc = new ProcessWorld(world);
+            var worldProc = proc.Execute();
+            string worldName = Path.GetFileName(args.Input);
+            File.WriteAllText(args.Output, worldProc.ToString());
+
+            //if (Directory.Exists(args.Input))
+            //{
+            //    string[] worlds = Directory.GetFiles(Path.GetFullPath(args.Input), "*.world");
+            //    foreach (string worldFile in worlds)
+            //    {
+            //        string worldRaw = File.ReadAllText(worldFile);
+            //        Models.World worldData = JsonSerializer.Deserialize<Models.World>(worldRaw);
+            //        Entities.World world = WorldMapper.Map(worldData);
+            //        world.Name = Path.GetFileNameWithoutExtension(worldFile);
+
+            //        world.GetMatrix();
+            //        ProcessWorld proc = new ProcessWorld(world);
+            //        var worldProc = proc.Execute();
+            //        string worldName = Path.GetFileName(worldFile);
+            //        File.WriteAllText(Path.Combine(args.Output, Path.GetFileNameWithoutExtension(worldName) + "_worldMap.inc"), worldProc.ToString());
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Path {args.Input} does not exist.");
+            //}
         }
     }
 }
